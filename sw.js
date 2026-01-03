71dd12d95c28f894762dd1980e8977170fc69e05
Rajesh Singh <rajeshnksingh@gmail.com>
2026-01-04 02:00:11 +0530

    Added PWA install option

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'kb-cache-v1';
+const PRECACHE_URLS = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
